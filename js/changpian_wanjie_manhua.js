37384ecf3f865b6c28ca3620b53eee6dd27e8457
duan <naonao800@163.com>
2018-05-06 00:01:38 +0800

    添加一些碎片

--- js/changpian_wanjie_manhua.js
+++ js/changpian_wanjie_manhua.js
@@ -15,7 +15,7 @@ window.addEventListener ("scroll", function (e) {
 var page_max_manhua_num = 20;
 
 // 漫画列表数量
-var manhua_list_num = 15;
+var manhua_list_num = 20;
 
 // 漫画列表总表
 var manhua_list = [];
